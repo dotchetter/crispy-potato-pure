8b366519fa1acc7349278f82ccd540956503afd7
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-17 22:01:06 +0200

    State added: switchLed
    switchLed function alternates between ON and OFF depending on
    the state of the led from last call.

--- inlupp1_embedded_systems/main.cpp
+++ inlupp1_embedded_systems/main.cpp
@@ -4,25 +4,12 @@
 #include "serial.h"
 #include "StateMachine.h"
 
+// State function pointer declarations
 void idle();
-StateMachine<char> sm = StateMachine<char>(0, (fp_t)&idle);
+void switchLed();
 
-void idle()
-{
-    sm.transitionTo(1);
-}
 
-void toggleLedOn(int pin)
-{
-    PORTB = 1 << pin | PORTB;
-    sm.release();
-}
-
-void toggleLedOff(int pin)
-{
-    PORTB = ~(1 << pin) & PORTB;
-    sm.release();
-}
+StateMachine<char> sm = StateMachine<char>(0, &idle);
 
 
 void setup()
@@ -36,16 +23,45 @@ void setup()
     BUTTON 1:   8  (PORTB BIT 0)
     BUTTON 2:   12 (PORTB BIT 4)
 
+
     DDRB MUST BE: 00001110 (0 TO LISTEN, 1 TO WRITE)
 */
 {
     // Set DDRB to listen to all pins except bit 1, 2, 3. (0 0 0 0 1 1 1 0)
-    DDRB = (7 << 1);
-    
-    sm.addState(1, (fp_t)&toggleLedOn);
-    sm.addState(2, (fp_t)&toggleLedOff);
-    sm.setChainedState(1, 2);
+    DDRB = (7 << 1);    
+    sm.addState(1, switchLed);
 }
+
+
+void idle()
+{
+    sm.transitionTo(1);
+}
+
+void toggleLedOn(int led)
+{
+    PORTB = 1 << led | PORTB;
+}
+
+void toggleLedOff(int led)
+{
+    PORTB = ~(1 << led) & PORTB;
+}
+
+void switchLed()
+{
+    static char next_direction;
+
+    switch (next_direction)
+    {
+        case 0: toggleLedOn(3); next_direction = 1; break;
+        case 1: toggleLedOff(3); next_direction = 0; break;
+    }
+
+    _delay_ms(1000);
+    sm.release();
+}
+
 
 int main ()
 {
