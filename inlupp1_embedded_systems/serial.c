a7d40dc5ca5d340ea9bad3f68cb824bd72480337
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-17 21:38:37 +0200

    Implemented methods to initialize, read from and listen to UART.
    TODO:
    
    Solve bug where unreadable messages are received from the device for unknown reasons.

--- inlupp1_embedded_systems/serial.c
+++ inlupp1_embedded_systems/serial.c
@@ -1,4 +1,47 @@
 #include <avr/io.h>
-#include <util/delay.h>
-
 #include "serial.h"
+
+
+void uart_init(unsigned int ubrr)
+{
+    // Left shift the UBRR0H Register by ubrr 8 positions
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+
+    // Enable receiver and transmitter registers
+    UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+
+    // Set the frame format, being 8N1
+    UCSR0C = (1<<USBS0) | (3<<UCSZ00);
+}
+
+void uart_putchar(unsigned char chr)
+{
+    /* 
+    Suspend and await equal state between 
+    registers to ensure empty transmit buffer 
+    */
+    while(!(UCSR0A & (1<<UDRE0)))
+    ;
+
+    UDR0 = chr;
+}
+
+void uart_putstr(const char *str)
+{
+    while (*str)
+    {
+        uart_putchar(*str);
+        str++;
+    }
+}
+
+char uart_getchar(void)
+{
+    // Await data to be received
+    while (!(UCSR0A & (1<<RXC0)))
+    ;
+    return UDR0;
+}
+
+//void uart_echo(void);
--- inlupp1_embedded_systems/serial.h
+++ inlupp1_embedded_systems/serial.h
@@ -1,14 +1,14 @@
 #ifndef SERIAL_H_
 #define SERIAL_H_
 
-void uart_init(void);
+#define F_OSC 16000000UL // Atmega 328P 16Mhz
+#define BAUD 115200UL
+#define MYUBRR (((F_OSC / (BAUD * 16UL))) -1)
 
-void uart_putchar(char chr);
+void uart_init(unsigned int ubrr);
+void uart_putchar(unsigned char chr);
 void uart_putstr(const char *str);
-
 char uart_getchar(void);
-
 void uart_echo(void);
 
 #endif
-
