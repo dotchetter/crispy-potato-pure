1f7dbd790866956f964ff6e3bbb0b77c013cd089
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-18 17:42:30 +0200

    trivial style refactor

--- inlupp1_embedded_systems/main.cpp
+++ inlupp1_embedded_systems/main.cpp
@@ -1,6 +1,3 @@
-#include <avr/io.h>
-#define F_CPU 16000000UL
-#include <util/delay.h>
 #include "serial.h"
 #include "StateMachine.h"
 #include <avr/io.h>
@@ -31,7 +28,9 @@ void setup()
 */
 {
     // Set DDRB to listen to all pins except bit 1, 2, 3. (0 0 0 0 1 1 1 0)
-    DDRB = (7 << 1);    
+    DDRB = (7 << 1);
+
+    // 
     sm.addState(1, switchLed);
 }
 
@@ -55,42 +54,33 @@ void switchLed()
 {
     static char next_direction;
 
-    switch (next_direction)
-    {
+    switch (next_direction) {
         case 0: toggleLedOn(3); next_direction = 1; break;
         case 1: toggleLedOff(3); next_direction = 0; break;
     }
 
-    _delay_ms(1000);
-    sm.release();
+   sm.release();
 }
 
-
-int main ()
-{
-    setup();
-
-    while(1)
-    {
-        sm.next()();
-    }
-    return 0;
-}
-
-
-
-
-
-
-
-
-// int main()
+// int main ()
 // {
-//     uart_init(MYUBRR);
-//    
+//     setup();
+
 //     while(1)
 //     {
-//         uart_putstr("Testar =)\r\n");
-//         _delay_ms(1000);
+//         sm.next()();
 //     }
-// }
+//     return 0;
+//}
+
+int main()
+{
+    uart_init();
+    setup();
+ 
+    while(1)
+    {
+        switchLed();
+        uart_echo();
+    }
+}
