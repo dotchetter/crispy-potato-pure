ef33f348b11a132cdb8c4df6355c26210b9eb3cf
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-18 17:41:30 +0200

    C++ compile compatibility with C includes

--- inlupp1_embedded_systems/serial.h
+++ inlupp1_embedded_systems/serial.h
@@ -1,14 +1,23 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
 #ifndef SERIAL_H_
 #define SERIAL_H_
 
-#define F_OSC 16000000UL // Atmega 328P 16Mhz
-#define BAUD 115200UL
-#define MYUBRR (((F_OSC / (BAUD * 16UL))) -1)
 
-void uart_init(unsigned int ubrr);
+#define F_CPU 16000000 // Atmega 328P 16Mhz
+#define BAUD 9600
+#define UBRR ((F_CPU/16/BAUD) - 1)
+
+void uart_init();
 void uart_putchar(unsigned char chr);
 void uart_putstr(const char *str);
-char uart_getchar(void);
+unsigned char uart_getchar(void);
 void uart_echo(void);
+void uart_getline(char* buf);
+const char serial_available();
 
+#endif
+#ifdef __cplusplus
+}
 #endif
