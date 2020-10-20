26ac95f7ef65731364440d6c54486b96f4a28f85
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-21 01:09:28 +0200

    changed return type -> const unsigned char

--- src/serial.h
+++ src/serial.h
@@ -9,10 +9,10 @@ extern "C" {
 #define BAUD 38400UL
 #define UBRR ((F_CPU/16/BAUD) - 1)
 
- void uart_init();
+void uart_init();
 void uart_putchar(unsigned char chr);
 void uart_putstr(const char *str);
-unsigned char uart_getchar(void);
+const unsigned char uart_getchar();
 void uart_echo(void);
 void uart_getline(char* command_buf, int size);
 const char serial_available();
