524ec98a659c6d1a2aa99e71f03f148d0d340517
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-21 03:10:08 +0200

    commands.h holds const char arrays saved in PROGMEM for memory conservation.
    These arrays are commands, parsed in the firmware from data over UART.

---
+++ src/commands.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef COMMANDS_H
+#define COMMANDS_H
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+
+const char command_on[] PROGMEM = "ON";
+const char command_off[] PROGMEM = "OFF";
+
+PGM_P const uart_command_table[] PROGMEM = 
+{   
+    command_on,
+    command_off,
+};
+#endif
