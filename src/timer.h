f58130b275cdd8fa5e33d156e80ab1fcffeedba6
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-27 18:54:57 +0100

    includes and defines in header file

--- src/timer.h
+++ src/timer.h
@@ -4,7 +4,12 @@ extern "C" {
 #ifndef _TIMER_H_
 #define _TIMER_H_
 
-void timer_init(void);
+#include <avr/io.h>
+#include <util/atomic.h>
+#include <avr/interrupt.h>
+
+uint32_t millis();
+void timer_init();
 
 #endif
 #ifdef __cplusplus
