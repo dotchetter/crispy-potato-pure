4452b1dbd3c33eb6d99d00c2aa8ca86536286926
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-28 18:41:03 +0100

    Added command to dim the crispy-sheild led. Too many hours of coding with this lit made me see stuff in green. Literally.

--- src/commands.h
+++ src/commands.h
@@ -4,10 +4,12 @@
 
 const char command_on[] PROGMEM = "ON";
 const char command_off[] PROGMEM = "OFF";
+const char command_die[] PROGMEM = "-";
 
 PGM_P const uart_command_table[] PROGMEM = 
 {   
     command_on,
     command_off,
+    command_die
 };
 #endif
