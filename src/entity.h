fed4b024b1970f6735727348c956ccb8f78ec7c0
Simon Olofsson <dotchetter@protonmail.ch>
2020-11-01 11:38:22 +0100

    Assignment 1.4.2:
     - Struct ENTITY
       The ENTITY struct is a template to represent any component
       that fits the mold. In this project it is used to represent
       buttons and LED components.
    
       * registry_bit
         the bit in the port that is for this entity
    
       * last_updated_ms
         a timestamp holder for when this entity was last updated/read/other
    
       * debounce_ms
         a defined amount of milliseconds used as a criteria to debounce
         the entity
    
       * long_press_trigger_ms
         a defined amount of milliseconds used as a criteria to identify
         a long press for the entity
    
       * is_active
         binary (1/0) to indicate whether this entity is currently classed
         as 'active' depending on the situation

--- src/entity.h
+++ src/entity.h
@@ -1,11 +1,21 @@
 #ifndef _ENTITY_H_
 #define _ENTITY_H_
+#include <avr/io.h>
 
-typedef struct
+typedef struct {
+    uint8_t registry_bit;
+    uint32_t last_updated_ms;
+    uint32_t debounce_ms;
+    uint32_t long_press_trigger_ms;
+    uint8_t is_active;
+    volatile uint8_t *port;
+}ENTITY;
+
+typedef enum
 {
-    char registry_bit;
-    unsigned long last_updated_ms;
-    char is_lit;
-}ENTITY_LED;
+    IDLE,
+    ACTIVE,
+    INACTIVE
+} state;
 
 #endif
