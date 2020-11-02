efb1523f34e27e253930f0d9396207d83a66cdeb
Simon Olofsson <dotchetter@protonmail.ch>
2020-11-03 00:11:18 +0100

    function declarations

--- src/timer.h
+++ src/timer.h
@@ -9,10 +9,12 @@ extern "C" {
 #include <avr/interrupt.h>
 
 volatile uint32_t timer2_ms;
+volatile uint8_t timer2_ctc_match;
 
-unsigned long millis();
+const uint8_t simple_ramp();
 void timer0_init();
 void timer2_init(uint32_t cpu_clk);
+unsigned long millis();
 
 #endif
 #ifdef __cplusplus
